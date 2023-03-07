dd9303e66087acb35ba9f6830dea98e55c19f25a
Mohammad Rahimi <rahimi.mhmmd@gmail.com>
2023-03-08 05:28:19 +0800

    fix: cppcheck in windows

--- cmake/CppCheck.cmake
+++ cmake/CppCheck.cmake
@@ -1,5 +1,5 @@
 function(AddCppCheck target)
-	find_program(CPPCHECK_PATH cppcheck REQUIRED PATHS "${CPPCHECK_PATH}")
+	find_program(CPPCHECK_PATH cppcheck REQUIRED PATHS "${CPPCHECK_INSTALL_DIR}")
 	set_target_properties(${target}
 		PROPERTIES CXX_CPPCHECK
 		"${CPPCHECK_PATH};--enable=warning;--error-exitcode=10"
