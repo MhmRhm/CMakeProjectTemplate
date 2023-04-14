092cc47163bdf2b1b958f31ca645cc2f0e268494
Mohammad Rahimi <rahimi.mhmmd@gmail.com>
2023-04-14 13:57:04 +0800

    build: enhance Format and Precompiling

--- cmake/Precompiling.cmake
+++ cmake/Precompiling.cmake
@@ -1,10 +1,7 @@
+include_guard(GLOBAL)
+
 add_library(precompiled INTERFACE)
 
 target_precompile_headers(precompiled INTERFACE
-	<iostream>
-	<string_view>
+	<cstddef>
 )
-
-macro(AddPrecompiledHeaders target)
-	target_link_libraries(${target} PRIVATE precompiled)
-endmacro()
